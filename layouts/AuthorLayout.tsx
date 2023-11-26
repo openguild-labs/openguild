c538b14d33320628778d16eeda18c82e1dbbd23c
tin-snowflake <cqtin0903@gmail.com>
2023-11-26 10:12:56 +0700

    update about

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -13,12 +13,7 @@ export default function AuthorLayout({ children, content }: Props) {
 
   return (
     <>
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            About
-          </h1>
-        </div>
+      <div className="divide-y divide-gray-200 py-5 dark:divide-gray-700">
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           <div className="flex flex-col items-center space-x-2 pt-8">
             {avatar && (
@@ -27,6 +22,7 @@ export default function AuthorLayout({ children, content }: Props) {
                 alt="avatar"
                 width={192}
                 height={192}
+                style={{ objectFit: 'cover' }}
                 className="h-48 w-48 rounded-full"
               />
             )}
